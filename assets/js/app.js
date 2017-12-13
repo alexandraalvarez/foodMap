8c73c3e62c9a33c4cd8177f589bb4380106b30c4
alexandraalvarez <alexandra.alvarez.a@gmail.com>
2017-12-13 15:53:13 -0300

    funcionalidad efecto hover sección food, con estilos, corregir responsive left y font size

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,5 +1,50 @@
-$(function(){
+$(document).ready(function(){
+	$("#img-chinese").hover(function(){ //hover para texto sobre las imágenes
+		$("#img-chinese").append("<span class='food-span'>Comida China</span>");
+		}, function(){
+			$(".food-span").remove();
+		});
+	$("#img-indian").hover(function(){ //hover para texto sobre las imágenes
+		$("#img-indian").append("<span class='food-span'>Comida Hindú</span>");
+		}, function(){
+			$(".food-span").remove();
+		});
+	$("#img-italian").hover(function(){ //hover para texto sobre las imágenes
+		$("#img-italian").append("<span class='food-span'>Comida Italiana</span>");
+		}, function(){
+			$(".food-span").remove();
+		});
+	$("#img-mexican").hover(function(){ //hover para texto sobre las imágenes
+		$("#img-mexican").append("<span class='food-span'>Comida Mexicana</span>");
+		}, function(){
+			$(".food-span").remove();
+		});
+	$("#img-japanese").hover(function(){ //hover para texto sobre las imágenes
+		$("#img-japanese").append("<span class='food-span'>Comida Japonesa</span>");
+		}, function(){
+			$(".food-span").remove();
+		});
+	$("#img-vegetarian").hover(function(){ //hover para texto sobre las imágenes
+		$("#img-vegetarian").append("<span class='food-span'>Comida Vegetariana</span>");
+		}, function(){
+			$(".food-span").remove();
+		});
+	$(function(){ //efecto splash
    setTimeout(function() {
       $('#sectn-splash').fadeOut(500);
    }, 3000);
-});
+	});
+});//final del ready function
+
+
+
+
+
+
+
+	
+
+
+
+
+//$('.elemento').toggle(); //para hacer aparecer y desaparecer las secciones con el filtro
