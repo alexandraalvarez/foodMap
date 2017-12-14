0e21f184b1e9954d6e2988caaf21339d83ebf5d4
alexandraalvarez <alexandra.alvarez.a@gmail.com>
2017-12-13 22:10:39 -0300

    funcion para rescatar el contenido del input, validarlo y que muestre la seccion elegida

--- assets/js/app.js
+++ assets/js/app.js
@@ -141,7 +141,39 @@ $(document).ready(function(){
 		});
 	//final del hover section vegetarian
 
-    
+  function validateFood () {
+	var validFood = ["china","hindú", "italiana", "mexicana", "japonesa", "vegetariana"];
+	var onlyLetters = /^[A-Za-z\_\-\.\s\xF1\xD1]+$/;
+		
+	$("#button-search").click(function(){
+		var inputSearch = $("#input-search").val();
+		if (inputSearch.length === 0) {
+			$(".input-group").append("<span class='alert'>Debe ingresar un tipo de comida</span>");
+			
+		} else if (!onlyLetters.test(inputSearch)) {
+			$(".input-group").append("<span class='alert'>Debe ingresar el tipo de comida con letras</span>");
+		} else {
+			$(".alert").remove();
+			$("#sectn-type-food").hide();//para esconder la sección de los tipos de comidas
+		} function searchFood() {
+			if (inputSearch == validFood[0]) {
+				$("#sectn-chinese").show();
+			} else if (inputSearch == validFood[1]){
+				$("#sectn-indian").show();
+			} else if (inputSearch == validFood[2]){
+				$("#sectn-italian").show();
+			} else if (inputSearch == validFood[3]){
+				$("#sectn-mexican").show();
+			} else if (inputSearch == validFood[4]){
+				$("#sectn-japanese").show();
+			} else if (inputSearch == validFood[4]){
+				$("#sectn-japanese").show();
+			} else if (inputSearch == validFood[5]){
+				$("#sectn-vegetarian").show();
+			} 
+		};
+	});
+};  
 
 });//final del ready function
 
